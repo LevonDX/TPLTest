4b1e8831c5824b46b756efaa1b63c68c1222db18
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-12-19 20:42:28 +0400

    Add project files.

---
+++ TPLTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace TPLTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Task<long> task = Task.Run(static () =>
+			{
+				long sum = 0;
+
+				for (int i = 0; i < 1E09; i++)
+					sum += i;
+
+				return sum;
+			});
+
+			Console.WriteLine("Task running...");
+
+			//long result = task.Result; // blocking method of getting result
+
+			//Console.WriteLine("Task completed. Result: {0}", result);
+
+			task.ContinueWith(t => CalcFinished(t));
+			Console.WriteLine("Tasl still running");
+			Console.ReadLine();
+		}
+
+
+		static void CalcFinished(Task<long> task)
+		{
+			Console.WriteLine("Task completed. Result: {0}", task.Result);
+		}
+	}
+}
